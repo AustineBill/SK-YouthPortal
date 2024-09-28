0b1e962dd06ee3b4664fba8fcb09a6841e99a683
Mark Jhay Corpuz <markjhaycorpuzz@gmail.com>
2024-09-28 15:56:37 +0800

    New Admin Navbar component
    This admin navbar is exclusive only for the admin of the website.

---
+++ src/Classes/AdminNavbar.jsx
@@ -0,0 +1,11 @@
+const AdminNavbar = () => {
+    return (
+        <nav className="admin-navbar">
+            <div className='admin-panel'>
+                <h1>iSKed - Admin Panel</h1>
+            </div>
+        </nav>
+    );
+}
+
+export default AdminNavbar;
