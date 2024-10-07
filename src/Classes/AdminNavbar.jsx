dfad660a2c564286db94d9363721fb320063a4a4
Mark Jhay Corpuz <tuazonmarivic31@gmail.com>
2024-10-07 23:48:21 +0800

    New signout button for admin side navigation bar

--- src/Classes/AdminNavbar.jsx
+++ src/Classes/AdminNavbar.jsx
@@ -1,9 +1,22 @@
+import { useNavigate } from "react-router-dom";
+
 const AdminNavbar = () => {
+    const navigate = useNavigate();
+
+    const handleSignout = () => {
+        console.log("Signed out");
+
+        navigate("/admin");
+    }
+
     return (
         <nav className="admin-navbar">
             <div className='admin-panel'>
                 <h1>iSKed - Admin Panel</h1>
             </div>
+            <button onClick={handleSignout}>
+                Sign out
+            </button>
         </nav>
     );
 }
--- src/Classes/UserAuthentication.jsx
+++ src/Classes/UserAuthentication.jsx
@@ -37,69 +37,62 @@ const UserAuthentication = ({ setIsAdminLoggedIn }) => {
     };
 
     return (
-        <div className="container-fluid d-flex mt-5">
-            <div className="col-lg-7 d-flex flex-column justify-content-center align-items-start p-4 ms-4">
-                {!isSignUpSuccessful && (
-                    <>
-                        <div className="text-center text-lg-start">
-                            <h1 className="display-4 fw-bold mb-2">Lagi't lagi para sa Kabataan,</h1>
-                        </div>
-                        <h1 className="SubText animated slideInRight">
-                            Barangay at sa Bayan. <span className="custom-name">iSKed</span>
-                        </h1>
-                        <p className="custom-intro-details">Western Bicutan</p>
-                    </>
-                )}
-            </div>
-
-            <div className="col-lg-4 d-flex align-items-center mt-5">
-                <div className="w-100">
-                    {view === 'signIn' && (
-                        <div className="sign-in-form">
-                            <form onSubmit={handleAdminLogin}>
-                                <div className='welcome-back-sign-in'>
-                                    <h2>Welcome back!</h2>
-                                </div>
-                                <div className='sign-in-username'>
-                                    <input type="text" name="username" placeholder='Username' required />
-                                </div>
-                                <div className='sign-in-password'>
-                                    <input type="password" name="password" placeholder='Password' required />
-                                </div>
-                                <div>
-                                    <Link to="/rawr" className="navbar-brand">
-                                        <p id='sign-in-forgot-password'>Forgot your password?</p> 
-                                    </Link>
-                                </div>
-                                <button type="submit">Sign In</button>
-                                <div className='sign-in-form-bottom'>
-                                    <p>Don’t have an account?</p>
-                                    <Link to="/userauth?view=signUp" className="navbar-brand">
-                                        <p id='sign-up-button'>Sign up</p>
-                                    </Link>
-                                </div>
-                            </form>
-                        </div>
-                    )}
-
-                    {view === 'signUp' && (
-                        <div className="sign-up-form">
-                            <form onSubmit={handleSignUpSubmit}>
-                                <div className='start-with-us-sign-up'>
-                                    <h2>Start with us!</h2>
-                                </div>
-                                <div>
-                                    <p>Lorem Ipsum</p>
-                                </div>
-                                <button type="submit">Sign Up</button>
-                            </form>
-                        </div>
-                    )}
+        <div className="auth-page">
+            {!isSignUpSuccessful && (
+                <div className='welcome'>
+                    <div className='welcome1'>
+                        <h1>iSKed</h1>
+                    </div>
+                    <div className='welcome2'>
+                        <p>Western Bicutan</p>
+                    </div>
                 </div>
-            </div>
+            )}
 
-            {/* Optional Success Message */}
-            {/* {isSignUpSuccessful && (
+            {view === 'signIn' && (
+                <div className="sign-in-form">
+                    <form onSubmit={handleAdminLogin}>
+                        <div className='welcome-back-sign-in'>
+                            <h2>Welcome back!</h2>
+                        </div>
+                        <div className='sign-in-username'>
+                            <input type="text" name="username" placeholder='Username' required />
+                        </div>
+                        <div className='sign-in-password'>
+                            <input type="password" name="password" placeholder='Password' required />
+                        </div>
+                        <div>
+                            <Link to="/rawr" className="navbar-brand">
+                                <p id='sign-in-forgot-password'>Forgot your password?</p>
+                            </Link>
+                        </div>
+                        <button type="submit">Sign In</button>
+                        <div className='sign-in-form-bottom'>
+                            <p>Don’t have an account?</p>
+                            <Link to="/userauth?view=signUp" className="navbar-brand">
+                                <p id='sign-up-button'>Sign up</p>
+                            </Link>
+                        </div>
+                    </form>
+                </div>
+            )}
+
+            {view === 'signUp' && (
+                <div className="sign-up-form">
+                    <form onSubmit={handleSignUpSubmit}>
+                        <div className='start-with-us-sign-up'>
+                            <h2>Start with us!</h2>
+                        </div>
+                        <div>
+                            <p>Lorem Ipsum</p>
+                        </div>
+                        <button type="submit">Sign Up</button>
+                    </form>
+                </div>
+            )}
+
+    {/* Optional Success Message */ }
+{/* {isSignUpSuccessful && (
                 <div className="success-message">
                     <h2>Your account is ready!</h2>
                     <p>Your profile information and login credentials have been successfully created. Click "Start Now" to explore the SK Youth program.</p>
@@ -108,7 +101,7 @@ const UserAuthentication = ({ setIsAdminLoggedIn }) => {
                     </form>
                 </div>
             )} */}
-        </div>
+        </div >
     );
 };
 
