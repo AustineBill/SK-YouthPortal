68ae6f25f7ae8fdb6074beb88f19e1cb2159cf6a
Mark Jhay Corpuz <144748958+Emjey007@users.noreply.github.com>
2024-10-14 16:08:23 +0800

    Updated Admin Sidebar
    Made changes at the code to make it clear and for the sidebar to function well.

--- src/Admin/AdminSidebar.jsx
+++ src/Admin/AdminSidebar.jsx
@@ -1,13 +1,10 @@
-import React from 'react';
+import { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './AdminSidebar.css'
 
 const AdminSidebar = () => {
-    const [openDropdown, setOpenDropdown] = React.useState(0);
-
-    const toggleDropdown = (index) => {
-        setOpenDropdown(openDropdown === index ? 0 : index);
-    };
+    const [manageWebsiteChildLinksOpen, setmanageWebsiteChildLinksOpen] = useState(false);
+    const [manageRequestsChildLinksOpen, setmanageRequestsChildLinksOpen] = useState(false);
 
     return (
         <div className="sidebar-container">
@@ -18,42 +15,43 @@ const AdminSidebar = () => {
                         <img src='#' alt="Home icon" />
                         <Link to="/admin-dashboard">Home</Link>
                     </li>
-                    <li className="clickable-link" onClick={() => toggleDropdown(1)}>
+
+                    <li className="manage-website-link-open" onClick={() => setmanageWebsiteChildLinksOpen(!manageWebsiteChildLinksOpen)}>
                         <img src='#' alt="Manage Website icon" />
                         Manage Website
-                        {openDropdown === 1 && (
-                            <ul className="dropdown">
-                                <li>
-                                    <img src='#' alt="Manage Home Page icon" />
-                                    <Link to="/manage-home-page">Manage Home Page</Link>
-                                </li>
-                                <li>
-                                    <img src='#' alt="Manage About Us Page icon" />
-                                    <Link to="/manage-about-us">Manage About Us Page</Link>
-                                </li>
-                                <li>
-                                    <img src='#' alt="Manage Program Page icon" />
-                                    <Link to="/manage-program">Manage Program Page</Link>
-                                </li>
-                                <li>
-                                    <img src='#' alt="Manage Contact Us Page icon" />
-                                    <Link to="/manage-contact-us">Manage Contact Us Page</Link>
-                                </li>
-                            </ul>
-                        )}
                     </li>
-                    <li className="clickable-link" onClick={() => toggleDropdown(2)}>
+                    {manageWebsiteChildLinksOpen && (
+                        <ul className="manage-website-nested-links">
+                            <li>
+                                <img src='#' alt="Manage Home Page icon" />
+                                <Link to="/manage-home-page">Manage Home Page</Link>
+                            </li>
+                            <li>
+                                <img src='#' alt="Manage About Us Page icon" />
+                                <Link to="/manage-about-us">Manage About Us Page</Link>
+                            </li>
+                            <li>
+                                <img src='#' alt="Manage Program Page icon" />
+                                <Link to="/manage-program">Manage Program Page</Link>
+                            </li>
+                            <li>
+                                <img src='#' alt="Manage Contact Us Page icon" />
+                                <Link to="/manage-contact-us">Manage Contact Us Page</Link>
+                            </li>
+                        </ul>
+                    )}
+                    <li className="manage-requests-link-open" onClick={() => setmanageRequestsChildLinksOpen(!manageRequestsChildLinksOpen)}>
                         <img src='#' alt="Manage Requests icon" />
                         Manage Requests
-                        {openDropdown === 2 && (
-                            <ul className="dropdown">
-                                <li>
-                                    <img src='#' alt="Reservation icon" />
-                                    <Link to="/admin/reservation">Reservation</Link>
-                                </li>
-                            </ul>
-                        )}
                     </li>
+                    {manageRequestsChildLinksOpen && (
+                        <ul className="manage-requests-nested-links">
+                            <li>
+                                <img src='#' alt="Reservation icon" />
+                                <Link to="/admin/reservation">Reservation</Link>
+                            </li>
+                        </ul>
+                    )}
                     <li>
                         <img src='#' alt="Reports icon" />
                         <Link to="/reports">Reports</Link>
@@ -69,4 +67,3 @@ const AdminSidebar = () => {
 };
 
 export default AdminSidebar;
-
