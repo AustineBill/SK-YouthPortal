34855e5af2ac036e295b344e5896d111a4b721f2
Mark Jhay Corpuz <144748958+Emjey007@users.noreply.github.com>
2024-10-18 16:56:17 +0800

    New codes for Manage Home Details in admin side.
    New content for Manage Home Details in admin side. The contents are still incomplete.

--- src/Admin/AdminManageHome.jsx
+++ src/Admin/AdminManageHome.jsx
@@ -1,9 +1,52 @@
+import { useState } from 'react';
+import { Link } from 'react-router-dom';
+
 const ManageHomePage = () => {
+    const [activeContent, setActiveContent] = useState('manageDetails');
+
     return (
         <div>
-            <h2>Manage Home Details</h2>
-        </div>
+            <ul>
+                <li onClick={() => setActiveContent('manageDetails')}>
+                    Manage Details
+                </li>
+                <li onClick={() => setActiveContent('allAnnouncementsAndEvents')}>
+                    All Announcements/Events
+                </li>
+                <li onClick={() => setActiveContent('addNew')}>
+                    Add New
+                </li>
+            </ul>
 
+            {/* Conditional Rendering of Components */}
+            <div className="component-contents">
+                {activeContent === 'manageDetails' && (
+                    <div>
+                        <h2>Manage Details</h2>
+                        <Link to="/admin/edit-home-details">
+                            <button>Edit Details</button>
+                        </Link>
+                    </div>
+                )}
+
+                {activeContent === 'allAnnouncementsAndEvents' && (
+                    <div>
+                        <h2>All Announcements/Events</h2>
+                    </div>
+                )}
+
+                {activeContent === 'addNew' && (
+                    <div>
+                        <h2>Add New</h2>
+                        <form>
+                            <input type="text" placeholder="Name of Announcement/Event" />
+                            <input type="text" placeholder="Blah" />
+                            <button>Submit</button>
+                        </form>
+                    </div>
+                )}
+            </div>
+        </div>
     );
 }
 
