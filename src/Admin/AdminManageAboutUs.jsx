c4928546580b750a8e5cc81b85a2b2c7abd4784a
Mark Jhay Corpuz <144748958+Emjey007@users.noreply.github.com>
2024-11-18 23:22:45 +0800

    Updated AdminManageAboutUs file
    Modified the current design of CSS file and updated the contents of JSX file to remove unnecessary codes.

--- src/Admin/AdminManageAboutUs.jsx
+++ src/Admin/AdminManageAboutUs.jsx
@@ -1,34 +1,35 @@
 import { useState } from 'react';
 import './styles/AdminManageAboutUs.css';
 
+const pageLabels = {
+    manageAboutDetails: 'Manage About Us Details',
+    editAboutDetails: 'Edit About Us Details'
+};
+
 const ManageAboutUs = () => {
     const [activeContent, setActiveContent] = useState('manageAboutDetails');
 
     return ( 
-        <div>
+        <div className='admin-about-us-container'>
+            <div className='label-and-button-container'>
+                <h2>{pageLabels[activeContent]}</h2>
+                
+                {activeContent !== 'manageAboutDetails' && (
+                    <button onClick={() => setActiveContent('manageAboutDetails')}>Back</button>
+                )}
+            </div>
+
             <div className='component-contents-container'>
                 {activeContent === 'manageAboutDetails' && (
                     <div className='about-us-container'>
-                        {/* <h2>Manage About Us Details</h2> */}
-                        <div className='about-us-text'>
-                            <h2>Manage About Us Details</h2>
-                        </div>
-                        <div className='about-us-details-container'>
-                            <button onClick={() => setActiveContent('editAboutDetails')}>Edit Details</button>
-                        </div>
+                        <button onClick={() => setActiveContent('editAboutDetails')}>Edit Details</button>
                     </div>
                 )}
 
                 {activeContent === 'editAboutDetails' && (
                     <div className='edit-about-us-container'>
-                        <div className='edit-about-us-text-and-button'>
-                            <h2>Edit About Us Details</h2>
-                            <button onClick={() => setActiveContent('manageAboutDetails')}>Back</button>
-                        </div>
-                        <div className='edit-about-us-details-container'>
-                            {/* Needs to be connected on database first to creata a logic and save the changes. */}
-                            <button onClick={() => setActiveContent('manageAboutDetails')}>Save Details</button>
-                        </div>
+                        {/* Needs to be connected on database first to creata a logic and save the changes. */}
+                        <button onClick={() => setActiveContent('manageAboutDetails')}>Save Details</button>
                     </div>
                 )}
             </div>
