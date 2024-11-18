3954cb1cdefdf36f9edee26589f4d3deb800998a
Mark Jhay Corpuz <144748958+Emjey007@users.noreply.github.com>
2024-11-18 23:31:03 +0800

    Updated AdminManageContactUs files
    Modified the current design and removed unnecessary codes to make it clear and easy to manage.

--- src/Admin/AdminManageContactUs.jsx
+++ src/Admin/AdminManageContactUs.jsx
@@ -1,30 +1,35 @@
 import { useState } from 'react';
 import './styles/AdminManageContactUs.css';
 
+const pageLabels = {
+    manageContactDetails: 'Manage Contact Details',
+    editContactDetails: 'Edit Contact Details'
+};
+
 const ManageContactUs = () => {
     const [activeContent, setActiveContent] = useState('manageContactDetails');
 
     return ( 
-        <div>
-            <div className="component-contents">
+        <div className='admin-contact-us-container'>
+            <div className='label-and-button-container'>
+            <h2>{pageLabels[activeContent]}</h2>
+                
+                {activeContent !== 'manageContactDetails' && (
+                    <button onClick={() => setActiveContent('manageContactDetails')}>Back</button>
+                )}
+            </div>
+
+            <div className="component-contents-container">
                 {activeContent === 'manageContactDetails' && (
-                    <div className='manage-contact-details-container'>
-                        <h2>Manage Contact Details</h2>
-                        <div className='current-contact-details-container'>
-                            <button onClick={() => setActiveContent('editContactDetails')}>Edit Details</button>
-                        </div>
+                    <div className='contact-us-container'>
+                        <button onClick={() => setActiveContent('editContactDetails')}>Edit Details</button>
                     </div>
                 )}
 
                 {activeContent === 'editContactDetails' && (
-                    <div className='edit-manage-contact-details-container'>
-                        <div className='about-us-text-and-back'>
-                            <h2>Edit Contact Details</h2>
-                            <button onClick={() => setActiveContent('manageContactDetails')}>Back</button>
-                        </div>
-                        <div className='edit-current-contact-details-container'>
-                            <button onClick={() => setActiveContent('manageContactDetails')}>Save Details</button>
-                        </div>
+                    <div className='edit-contact-us-container'>
+                        {/* Needs to be connected on database first to creata a logic and save the changes. */}
+                        <button onClick={() => setActiveContent('manageContactDetails')}>Save Details</button>
                     </div>
                 )}
             </div>
