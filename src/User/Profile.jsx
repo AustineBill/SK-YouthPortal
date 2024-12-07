0f2b10a6dd77263fd73e700e090f83f30894419a
AustineBill <auztine.malic@gmail.com>
2024-12-07 16:05:37 +0800

    Fix the Profile Display

--- src/User/Profile.jsx
+++ src/User/Profile.jsx
@@ -7,7 +7,6 @@ import {
   Card,
   Button,
   Breadcrumb,
-  ListGroup,
   Form,
 } from 'react-bootstrap';
 import Avatar from 'react-avatar';
@@ -15,28 +14,33 @@ import Avatar from 'react-avatar';
 const ProfilePage = () => {
   const [isEditing, setIsEditing] = useState(false);
   const [profileInfo, setProfileInfo] = useState(null);
-  const { username } = useParams(); // Corrected useParams invocation
+  const { username } = useParams(); 
   const navigate = useNavigate();
 
   useEffect(() => {
-
     const fetchProfile = async () => {
+      const username = sessionStorage.getItem('username');
+      console.log('Fetching profile for:', username);
+  
       try {
-        const response = await fetch(`http://localhost:5000/Profile/${username}?_=${new Date().getTime()}`);
-        console.log('Response Status:', response.status); // Check response status
-        
+        const response = await fetch(`http://localhost:5000/Profile/${username}`);
+  
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
+  
         const userData = await response.json();
+        console.log('Profile Data:', userData);
+  
         setProfileInfo(userData);
       } catch (error) {
-        console.error('Error fetching profile:', error); // Handle any errors here
+        console.error('Error fetching profile:', error);
       }
     };
+  
     if (username) fetchProfile();
   }, [username]);
-
+  
   
   if (!profileInfo && profileInfo !== null) {
     return <div>Loading...</div>; // While waiting for profileInfo to load
@@ -44,6 +48,16 @@ const ProfilePage = () => {
 
   const handleEditClick = () => {
     setIsEditing(!isEditing);
+  };
+
+  const formatDate = (dateString) => {
+    if (!dateString) return "N/A"; // Handle null or undefined dates
+    const date = new Date(dateString);
+    return new Intl.DateTimeFormat("en-US", {
+      month: "long",
+      day: "numeric",
+      year: "numeric",
+    }).format(date);
   };
 
   return (
@@ -62,71 +76,17 @@ const ProfilePage = () => {
           <Col lg="4">
             <Card className="mb-4 text-center">
               <Card.Body>
-                <Avatar className="m-2" name={profileInfo?.username} round={true} size="150" />
+                <Avatar className="m-2" name={profileInfo?.username} round={true} size="150"  style={{ text: "40px"}}/> 
                 <Card.Text className="m-2 fs-2">{profileInfo?.username}</Card.Text>
-                <Card.Text className="text-muted mb-1">Full Stack Developer</Card.Text>
-                <Card.Text className="text-muted mb-4">{profileInfo?.address}</Card.Text>
+                <Card.Text className="text-muted mb-1">{profileInfo?.educational_background}</Card.Text>
+                
+
                 <div className="d-flex justify-content-center mb-2">
                   <Button variant="outline-primary btn-block" className="ms-1">Change password</Button>
                   <Button variant="outline-secondary btn-block" className="ms-1" onClick={handleEditClick}>
                     {isEditing ? 'Cancel' : 'Edit information'}
                   </Button>
                 </div>
-              </Card.Body>
-            </Card>
-
-            <Card className="mb-4 mb-lg-0">
-              <Card.Body className="p-0">
-                <ListGroup variant="flush">
-                  <ListGroup.Item className="d-flex justify-content-between align-items-center p-3">
-                    <i className="bi bi-facebook fs-1"></i>
-                    {isEditing ? (
-                      <Form.Control
-                        type="text"
-                        name="facebook"
-                        defaultValue={profileInfo?.facebook}
-                      />
-                    ) : (
-                      <Card.Text>{profileInfo?.facebook}</Card.Text>
-                    )}
-                  </ListGroup.Item>
-                  <ListGroup.Item className="d-flex justify-content-between align-items-center p-3">
-                    <i className="bi bi-twitter-x fs-1"></i>
-                    {isEditing ? (
-                      <Form.Control
-                        type="text"
-                        name="twitter"
-                        defaultValue={profileInfo?.twitter}
-                      />
-                    ) : (
-                      <Card.Text>{profileInfo?.twitter}</Card.Text>
-                    )}
-                  </ListGroup.Item>
-                  <ListGroup.Item className="d-flex justify-content-between align-items-center p-3">
-                    <i className="bi bi-instagram fs-1"></i>
-                    {isEditing ? (
-                      <Form.Control
-                        type="text"
-                        name="instagram"
-                        defaultValue={profileInfo?.instagram}
-                      />
-                    ) : (
-                      <Card.Text>{profileInfo?.instagram}</Card.Text>
-                    )}
-                  </ListGroup.Item>
-                  <ListGroup.Item className="d-flex justify-content-between align-items-center p-3">
-                    <i className="bi bi-linkedin fs-1"></i>
-                    {isEditing ? (
-                      <Form.Control
-                        type="text"
-                        name="linkedin"
-                        defaultValue={profileInfo?.linkedin}
-                      />
-                    ) : (
-                      <Card.Text>{profileInfo?.linkedin}</Card.Text>
-                    )}
-                  </ListGroup.Item>
-                </ListGroup>
               </Card.Body>
             </Card>
           </Col>
@@ -141,7 +101,7 @@ const ProfilePage = () => {
                       <Card.Text>Full Name</Card.Text>
                     </Col>
                     <Col sm="9">
-                      <Card.Text className="text-muted">{profileInfo?.username}</Card.Text>
+                      <Card.Text className="text-muted">{profileInfo?.firstname} {profileInfo?.lastname}</Card.Text>
                     </Col>
                   </Row>
                   <hr />
@@ -149,17 +109,59 @@ const ProfilePage = () => {
                     <Col sm="3">
                       <Card.Text>Age</Card.Text>
                     </Col>
-                    <Col sm="9">
+                    <Col sm="3">
                       <Card.Text className="text-muted">{profileInfo?.age}</Card.Text>
+                    </Col>
+                    <Col sm="3">
+                      <Card.Text>Sex</Card.Text>
+                    </Col>
+                    <Col sm="3">
+                      <Card.Text className="text-muted">{profileInfo?.sex}</Card.Text>
+                    </Col>
+                  </Row>
+                  <hr />
+                  <Row>
+                  <Col sm="3">
+                      <Card.Text>Birthday</Card.Text>
+                    </Col>
+                    <Col sm="3">
+                      <Card.Text className="text-muted">{formatDate(profileInfo?.birthday)}</Card.Text>
+                    </Col>
+                    <Col sm="3">
+                      <Card.Text>Phone</Card.Text>
+                    </Col>
+                    <Col sm="3">
+                      <Card.Text className="text-muted">{profileInfo?.contact_number}</Card.Text>
                     </Col>
                   </Row>
                   <hr />
                   <Row>
                     <Col sm="3">
-                      <Card.Text>Sex</Card.Text>
+                      <Card.Text>Civil Status</Card.Text>
                     </Col>
-                    <Col sm="9">
-                      <Card.Text className="text-muted">{profileInfo?.sex}</Card.Text>
+                    <Col sm="3">
+                      <Card.Text className="text-muted">{profileInfo?.civil_status}</Card.Text>
+                    </Col>
+                    <Col sm="3">
+                      <Card.Text>Work Status</Card.Text>
+                    </Col>
+                    <Col sm="3">
+                      <Card.Text className="text-muted">{profileInfo?.work_status}</Card.Text>
+                    </Col>
+                  </Row>
+                  <hr />
+                  <Row>
+                    <Col sm="3">
+                      <Card.Text> Youth Age Group</Card.Text>
+                    </Col>
+                    <Col sm="3">
+                      <Card.Text className="text-muted">{profileInfo?.youth_age_group}</Card.Text>
+                    </Col>
+                    <Col sm="5">
+                      <Card.Text>Registered SK Voter</Card.Text>
+                    </Col>
+                    <Col sm="3">
+                      <Card.Text className="text-muted">{profileInfo?.registered_sk_voter}</Card.Text>
                     </Col>
                   </Row>
                   <hr />
@@ -168,25 +170,7 @@ const ProfilePage = () => {
                       <Card.Text>Email</Card.Text>
                     </Col>
                     <Col sm="9">
-                      <Card.Text className="text-muted">{profileInfo?.email}</Card.Text>
-                    </Col>
-                  </Row>
-                  <hr />
-                  <Row>
-                    <Col sm="3">
-                      <Card.Text>Phone</Card.Text>
-                    </Col>
-                    <Col sm="9">
-                      <Card.Text className="text-muted">{profileInfo?.contact_number}</Card.Text>
-                    </Col>
-                  </Row>
-                  <hr />
-                  <Row>
-                    <Col sm="3">
-                      <Card.Text>Country</Card.Text>
-                    </Col>
-                    <Col sm="9">
-                      <Card.Text className="text-muted">{profileInfo?.country}</Card.Text>
+                      <Card.Text className="text-muted">{profileInfo?.email_address}</Card.Text>
                     </Col>
                   </Row>
                   <hr />
@@ -195,15 +179,23 @@ const ProfilePage = () => {
                       <Card.Text>Address</Card.Text>
                     </Col>
                     <Col sm="9">
-                      <Card.Text className="text-muted">{profileInfo?.address}</Card.Text>
+                      <Card.Text className="text-muted"> {profileInfo?.barangay} {profileInfo?.city} {profileInfo?.zone} 
+                      </Card.Text>
+                    </Col>
+                  </Row>
+                  <hr />
+                  <Row>
+                    <Col sm="3">
+                      <Card.Text>Province</Card.Text>
+                    </Col>
+                    <Col sm="9">
+                      <Card.Text className="text-muted">{profileInfo?.province} {profileInfo?.regiopn} 
+                      </Card.Text>
                     </Col>
                   </Row>
                 </Card.Body>
               </Card>
             </fieldset>
-
-
-
 
             <fieldset className="border rounded mb-4 p-3">
               <legend className="p-2 rounded">More Information</legend>
