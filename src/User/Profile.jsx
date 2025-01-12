4ab80759b7e9238d380d1ca8dcc915ad59f8afdc
AustineBill <auztine.malic@gmail.com>
2025-01-13 03:28:47 +0800

    Fix Change Pasword

--- src/User/Profile.jsx
+++ src/User/Profile.jsx
@@ -1,5 +1,5 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
 import {
   Container,
   Row,
@@ -10,73 +10,81 @@ import {
   Modal,
   Form,
   Alert,
-} from 'react-bootstrap';
-import Avatar from 'react-avatar';
+} from "react-bootstrap";
+import Avatar from "react-avatar";
 
-const ProfilePage = () => {;
+const ProfilePage = () => {
   const [profileInfo, setProfileInfo] = useState(null);
   const [showModal, setShowModal] = useState(false);
-  const [oldPassword, setOldPassword] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [message, setMessage] = useState('');
-  const { username } = useParams(); 
+  const [showSuccessModal, setShowSuccessModal] = useState(false);
+  const [oldPassword, setOldPassword] = useState("");
+  const [newPassword, setNewPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [message, setMessage] = useState("");
+  const [successMessage, setSuccessMessage] = useState("");
+  const { username } = useParams();
   const navigate = useNavigate();
 
   useEffect(() => {
     const fetchProfile = async () => {
-      const username = sessionStorage.getItem('username');
+      const username = sessionStorage.getItem("username");
       //console.log('Fetching profile for:', username);
       try {
-        const response = await fetch(`http://localhost:5000/Profile/${username}`);
-  
+        const response = await fetch(
+          `http://localhost:5000/Profile/${username}`
+        );
+
         if (!response.ok) {
           throw new Error(`HTTP error! status: ${response.status}`);
         }
-  
+
         const userData = await response.json();
         setProfileInfo(userData);
       } catch (error) {
-        console.error('Error fetching profile:', error);
+        console.error("Error fetching profile:", error);
       }
     };
-  
+
     if (username) fetchProfile();
   }, [username]);
 
   const handlePasswordChange = async () => {
     if (newPassword !== confirmPassword) {
-      setMessage('New password and confirm password do not match.');
+      setMessage("New password and confirm password do not match.");
       return;
     }
-  
+
     try {
-      const id = sessionStorage.getItem('userId'); // Ensure user ID is stored in sessionStorage during login
-      const response = await fetch('http://localhost:5000/change-password', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ id, oldPassword, newPassword }),
-      });
-  
+      const id = sessionStorage.getItem("userId"); // Ensure user ID is stored in sessionStorage during login
+      const response = await fetch(
+        "http://localhost:5000/change-password-only",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ id, oldPassword, newPassword }),
+        }
+      );
+
       const data = await response.json();
-  
+
       if (!response.ok) {
-        setMessage(data.message || 'Error updating password');
+        setMessage(data.message || "Error updating password");
       } else {
-        setMessage('Password updated successfully');
-        setOldPassword('');
-        setNewPassword('');
-        setConfirmPassword('');
-        setShowModal(false);
+        setSuccessMessage("Password updated successfully!"); // This triggers the success message
+        setMessage(""); // Clear any error message
+        setOldPassword(""); // Reset the form fields
+        setNewPassword("");
+        setConfirmPassword("");
+        setShowModal(false); // Close the modal after success
+        setShowSuccessModal(true);
       }
     } catch (error) {
-      console.error('Error updating password:', error);
-      setMessage('Error updating password');
+      console.error("Error updating password:", error);
+      setMessage("Error updating password");
     }
   };
-  
 
   const formatDate = (dateString) => {
     if (!dateString) return "N/A"; // Handle null or undefined dates
@@ -88,7 +96,6 @@ const ProfilePage = () => {;
     }).format(date);
   };
 
-
   if (!profileInfo && profileInfo !== null) {
     return <div>Loading...</div>; // While waiting for profileInfo to load
   }
@@ -98,12 +105,14 @@ const ProfilePage = () => {;
   };
 
   return (
-    <section style={{ backgroundColor: '#eee' }}>
+    <section style={{ backgroundColor: "#eee" }}>
       <Container className="py-5">
         <Row>
           <Col>
             <Breadcrumb className="bg-light rounded-3 p-3 mb-4">
-              <Breadcrumb.Item onClick={() => navigate('/Dashboard')}>Home</Breadcrumb.Item>
+              <Breadcrumb.Item onClick={() => navigate("/Dashboard")}>
+                Home
+              </Breadcrumb.Item>
               <Breadcrumb.Item active>User Profile</Breadcrumb.Item>
             </Breadcrumb>
           </Col>
@@ -113,13 +122,28 @@ const ProfilePage = () => {;
           <Col lg="4">
             <Card className="mb-4 text-center">
               <Card.Body>
-                <Avatar className="m-2" name={profileInfo?.username} round={true} size="150"  style={{ text: "40px"}}/> 
-                <Card.Text className="m-2 fs-2">{profileInfo?.username}</Card.Text>
-                <Card.Text className="text-muted mb-1">{profileInfo?.educational_background}</Card.Text>
-                
+                <Avatar
+                  className="m-2"
+                  name={profileInfo?.username}
+                  round={true}
+                  size="150"
+                  style={{ text: "40px" }}
+                />
+                <Card.Text className="m-2 fs-2">
+                  {profileInfo?.username}
+                </Card.Text>
+                <Card.Text className="text-muted mb-1">
+                  {profileInfo?.educational_background}
+                </Card.Text>
 
                 <div className="d-flex justify-content-center mb-2">
-                  <Button variant="outline-primary btn-block" className="ms-1" onClick={ChangeModal} >Change password</Button>
+                  <Button
+                    variant="outline-primary btn-block"
+                    className="ms-1"
+                    onClick={ChangeModal}
+                  >
+                    Change password
+                  </Button>
                 </div>
               </Card.Body>
             </Card>
@@ -135,7 +159,9 @@ const ProfilePage = () => {;
                       <Card.Text>Full Name</Card.Text>
                     </Col>
                     <Col sm="9">
-                      <Card.Text className="text-muted">{profileInfo?.firstname} {profileInfo?.lastname}</Card.Text>
+                      <Card.Text className="text-muted">
+                        {profileInfo?.firstname} {profileInfo?.lastname}
+                      </Card.Text>
                     </Col>
                   </Row>
                   <hr />
@@ -144,28 +170,36 @@ const ProfilePage = () => {;
                       <Card.Text>Age</Card.Text>
                     </Col>
                     <Col sm="3">
-                      <Card.Text className="text-muted">{profileInfo?.age}</Card.Text>
+                      <Card.Text className="text-muted">
+                        {profileInfo?.age}
+                      </Card.Text>
                     </Col>
                     <Col sm="3">
                       <Card.Text>Sex</Card.Text>
                     </Col>
                     <Col sm="3">
-                      <Card.Text className="text-muted">{profileInfo?.sex}</Card.Text>
+                      <Card.Text className="text-muted">
+                        {profileInfo?.sex}
+                      </Card.Text>
                     </Col>
                   </Row>
                   <hr />
                   <Row>
-                  <Col sm="3">
+                    <Col sm="3">
                       <Card.Text>Birthday</Card.Text>
                     </Col>
                     <Col sm="3">
-                      <Card.Text className="text-muted">{formatDate(profileInfo?.birthday)}</Card.Text>
+                      <Card.Text className="text-muted">
+                        {formatDate(profileInfo?.birthday)}
+                      </Card.Text>
                     </Col>
                     <Col sm="3">
                       <Card.Text>Phone</Card.Text>
                     </Col>
                     <Col sm="3">
-                      <Card.Text className="text-muted">{profileInfo?.contact_number}</Card.Text>
+                      <Card.Text className="text-muted">
+                        {profileInfo?.contact_number}
+                      </Card.Text>
                     </Col>
                   </Row>
                   <hr />
@@ -174,13 +208,17 @@ const ProfilePage = () => {;
                       <Card.Text>Civil Status</Card.Text>
                     </Col>
                     <Col sm="3">
-                      <Card.Text className="text-muted">{profileInfo?.civil_status}</Card.Text>
+                      <Card.Text className="text-muted">
+                        {profileInfo?.civil_status}
+                      </Card.Text>
                     </Col>
                     <Col sm="3">
                       <Card.Text>Work Status</Card.Text>
                     </Col>
                     <Col sm="3">
-                      <Card.Text className="text-muted">{profileInfo?.work_status}</Card.Text>
+                      <Card.Text className="text-muted">
+                        {profileInfo?.work_status}
+                      </Card.Text>
                     </Col>
                   </Row>
                   <hr />
@@ -189,13 +227,17 @@ const ProfilePage = () => {;
                       <Card.Text> Youth Age Group</Card.Text>
                     </Col>
                     <Col sm="3">
-                      <Card.Text className="text-muted">{profileInfo?.youth_age_group}</Card.Text>
+                      <Card.Text className="text-muted">
+                        {profileInfo?.youth_age_group}
+                      </Card.Text>
                     </Col>
                     <Col sm="5">
                       <Card.Text>Registered SK Voter</Card.Text>
                     </Col>
                     <Col sm="3">
-                      <Card.Text className="text-muted">{profileInfo?.registered_sk_voter}</Card.Text>
+                      <Card.Text className="text-muted">
+                        {profileInfo?.registered_sk_voter}
+                      </Card.Text>
                     </Col>
                   </Row>
                   <hr />
@@ -204,7 +246,9 @@ const ProfilePage = () => {;
                       <Card.Text>Email</Card.Text>
                     </Col>
                     <Col sm="9">
-                      <Card.Text className="text-muted">{profileInfo?.email_address}</Card.Text>
+                      <Card.Text className="text-muted">
+                        {profileInfo?.email_address}
+                      </Card.Text>
                     </Col>
                   </Row>
                   <hr />
@@ -213,7 +257,10 @@ const ProfilePage = () => {;
                       <Card.Text>Address</Card.Text>
                     </Col>
                     <Col sm="9">
-                      <Card.Text className="text-muted"> {profileInfo?.barangay} {profileInfo?.city} {profileInfo?.zone} 
+                      <Card.Text className="text-muted">
+                        {" "}
+                        {profileInfo?.barangay} {profileInfo?.city}{" "}
+                        {profileInfo?.zone}
                       </Card.Text>
                     </Col>
                   </Row>
@@ -223,7 +270,8 @@ const ProfilePage = () => {;
                       <Card.Text>Province</Card.Text>
                     </Col>
                     <Col sm="9">
-                      <Card.Text className="text-muted">{profileInfo?.province} {profileInfo?.region} 
+                      <Card.Text className="text-muted">
+                        {profileInfo?.province} {profileInfo?.region}
                       </Card.Text>
                     </Col>
                   </Row>
@@ -234,6 +282,14 @@ const ProfilePage = () => {;
         </Row>
       </Container>
 
+      {/* Display success message */}
+      {successMessage && (
+        <Alert variant="success" className="text-center mt-3">
+          {successMessage}
+        </Alert>
+      )}
+
+      {/* Password Change Modal */}
       <Modal show={showModal} onHide={() => setShowModal(false)}>
         <Modal.Header closeButton>
           <Modal.Title>Change Password</Modal.Title>
@@ -271,11 +327,6 @@ const ProfilePage = () => {;
               />
             </Form.Group>
           </Form>
-          <div className="mt-3 text-center">
-            <Button variant="link" onClick={() => navigate('/forgot-password')}>
-              Forgot Password?
-            </Button>
-          </div>
         </Modal.Body>
         <Modal.Footer>
           <Button variant="secondary" onClick={() => setShowModal(false)}>
@@ -287,11 +338,20 @@ const ProfilePage = () => {;
         </Modal.Footer>
       </Modal>
 
-
+      <Modal show={showSuccessModal} onHide={() => setShowSuccessModal(false)}>
+        <Modal.Header closeButton>
+          <Modal.Title>Password Changed Successfully</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Alert variant="success">{successMessage}</Alert>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={() => setShowSuccessModal(false)}>
+            Close
+          </Button>
+        </Modal.Footer>
+      </Modal>
     </section>
   );
 };
-
 export default ProfilePage;
-
-
