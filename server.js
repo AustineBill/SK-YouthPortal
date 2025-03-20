e11a04ff4edc9309f77b332357ea1f210737d70c
AustineBill <auztine.malic@gmail.com>
2025-03-20 22:14:49 +0800

    Update server.js

--- server.js
+++ server.js
@@ -26,8 +26,8 @@ const pool = new Pool({
   user: process.env.DB_USER,
   host: process.env.DB_HOST,
   database: process.env.DB_NAME,
-  password: process.env.PASSWORD,
-  port: process.env.PORT,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
   ssl: {
     rejectUnauthorized: false,
   },
